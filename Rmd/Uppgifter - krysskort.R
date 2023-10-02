704c9365ce1be4b524fb7a071d20f57cdfbbf3e4
adamflr <flr.adam@gmail.com>
2023-10-02 09:19:20 +0200

    Language changes

--- Rmd/Uppgifter - krysskort.R
+++ Rmd/Uppgifter - krysskort.R
@@ -6,12 +6,15 @@ tab <- expand_grid(Question = 1:30 + rep(1:6, each = 5),
          Type = ifelse(Rank %in% 1:20, "Sal", 
                        ifelse(Rank %in% 21:25, "Bonus", "Hemuppgift")))
 
-g <- ggplot(tab, aes(y = -Question, Exercise, color = Type)) +
+tab2 <- tab %>% filter(Type != "Hemuppgift")
+
+g <- ggplot(tab2, aes(y = -Question, Exercise, color = Type)) +
   geom_point(shape = 21, size = 9.5, stroke = 1) +
   # geom_vline(xintercept = seq(5.5, 30.5, 5)) +
   annotate("text", y = -0.9, x = unique(tab$Exercise), label = paste0("", 1:length(unique(tab$Exercise))), family = "serif") +
   # annotate("text", x = c(25.5, 31.5), y = max(tab$Exercise) + 0.5, label = c("Bonus", "Hem"), family = "serif", hjust = 0) +
   cowplot::theme_nothing() +
+  scale_color_manual(values = c("purple", "hotpink")) +
   xlim(-2, 7)
 g
 
